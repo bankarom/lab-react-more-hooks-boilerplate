f97ef685e3854d2f5fce8a9b08ff4a959f775da0
bankarom <146808698+bankarom@users.noreply.github.com>
2024-02-25 16:02:02 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import TaskList from './components/Tasklist'
+import './App.css'
+import React, { Component } from 'react'
+
+function App() {
+  
+    return (
+      <TaskList/>
+    )
+  
+}
+
+
+
+
+export default App
